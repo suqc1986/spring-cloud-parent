de86abc80a1c2fcd2f8213f0bc70beecdd679d56
suqch <suqch@yonyou.com>
2020-09-14 09:03:11 +0800

    更新parent。pom.xml

--- hystrix-dashboard-demo-9011/src/main/java/com/biancheng/DashboardApplication.java
+++ hystrix-dashboard-demo-9011/src/main/java/com/biancheng/DashboardApplication.java
@@ -2,10 +2,14 @@ package com.biancheng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
-@EnableHystrixDashboard
+//@EnableHystrixDashboard
+@EnableTurbine
+@EnableEurekaClient
 public class DashboardApplication {
     public static void main(String[] args) {
         SpringApplication.run(DashboardApplication.class, args);
