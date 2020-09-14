021607c7f62b7c29779bd45c960e774eaa85fa05
suqch <suqch@yonyou.com>
2020-09-14 09:42:02 +0800

    整理pom依赖，公共依赖放在parent工程里

--- hystrix-dashboard-demo-9011/src/main/java/com/biancheng/DashboardApplication.java
+++ hystrix-dashboard-demo-9011/src/main/java/com/biancheng/DashboardApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
-//@EnableHystrixDashboard
+@EnableHystrixDashboard
 @EnableTurbine
 @EnableEurekaClient
 public class DashboardApplication {
